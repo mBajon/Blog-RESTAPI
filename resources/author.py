9ee81c0792795b67fea9edcb4a6211175be5d6ca
mbajon <maciej.bajon@wp.pl>
2020-05-07 23:40:45 +0200

    added 2 helpers to author resource

--- resources/author.py
+++ resources/author.py
@@ -2,11 +2,18 @@ from models.author import AuthorModel
 from flask_jwt import JWT, jwt_required
 from flask_restful import Resource, reqparse
 
+def _author(name):
+    return AuthorModel.filter_by_name(name)
+
+def _author_json(name):
+    author = AuthorModel.filter_by_name(name)
+    return author.json()
+
 class Author(Resource):
     parser = reqparse.RequestParser()
 
     def post(self, name):
-        if AuthorModel.filter_by_name(name):
+        if _author(name):
             return {"message":"author with that name already exists"},409
 
         data=Author.parser.parse_args()
@@ -20,15 +27,14 @@ class Author(Resource):
         return author.json(), 201   
     
     def get(self, name):
-        author = AuthorModel.filter_by_name(name)
-        if author:
-            return author.json(),200
+        
+        if _author(name):
+            return _author_json(name),200
         return {"message":"No author with that name"},404 
 
     def delete(self, name):
-        author=AuthorModel.filter_by_name(name)
-        if author:
-            author.delete_from_db()
+        if _author:
+            _author.delete_from_db()
                     
         return {"message":"Author:{} was deleted".format(name)},200
 
