ebc03e8bd6f97e71b497024ee0320d6182e263f1
mbajon <maciej.bajon@wp.pl>
2020-05-09 09:24:31 +0200

    added user model

---
+++ models/user.py
@@ -0,0 +1,26 @@
+from db import db
+
+class User(db.Model):
+    __tablename__ = "Users"
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username=username
+        self.password=password
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit(self)
+
+    def delete_from_db(self):
+        db.session.add(self)
+        db.session.commit(self)
+    
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
